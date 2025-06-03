6633ddef1b7c729964cf8874ea80c161543bd0ee
Oleg Shapoval <olegkazhan@gmail.com>
2025-06-03 13:54:38 +0300

    initial

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/x-team-eco-tote/',
     build: {
       sourcemap: true,
       rollupOptions: {
